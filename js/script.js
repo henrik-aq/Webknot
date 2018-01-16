180312398a23330bccb4f56cf32e0ad204c04393
leolainen <leolainen2@gmail.com>
2018-01-16 18:08:09 +0100

    hamburger color animation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 var navHamburger  = document.getElementById("nav-hamburger");
+var navHamburgerLines = document.querySelectorAll("#nav-hamburger div");
 var navMenu       = document.getElementById("nav-menu");
 
+console.log(navHamburgerLines);
+
 navHamburger.addEventListener("click", function() {
+  for (let i=0; i < navHamburgerLines.length; i++) {
+    navHamburgerLines[i].classList.toggle("hamburger-is-active");
+  }
+  
   if(navMenu.classList.contains("nav-is-active")) {
     navMenu.classList.add("nav-not-active");
     navMenu.classList.remove("nav-is-active");
