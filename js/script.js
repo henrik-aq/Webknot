8e24b0e34d15147f056e8001bba03e85d372336e
leolainen <leolainen2@gmail.com>
2018-01-16 20:39:44 +0100

    finished header for mobile, tablet, desktop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-var navHamburger  = document.getElementById("nav-hamburger");
+var navHamburger      = document.getElementById("nav-hamburger");
 var navHamburgerLines = document.querySelectorAll("#nav-hamburger div");
-var navMenu       = document.getElementById("nav-menu");
+var navMenu           = document.getElementById("nav-menu");
+var navMenuAnchor     = document.querySelectorAll("#nav-menu a");
 
 console.log(navHamburgerLines);
 
-navHamburger.addEventListener("click", function() {
+navHamburger.addEventListener("click", toggleMenu);
+for (var i = 0; i < navMenuAnchor.length; i++) {
+  navMenuAnchor[i].addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
   for (let i=0; i < navHamburgerLines.length; i++) {
     navHamburgerLines[i].classList.toggle("hamburger-is-active");
   }
-  
   if(navMenu.classList.contains("nav-is-active")) {
     navMenu.classList.add("nav-not-active");
     navMenu.classList.remove("nav-is-active");
@@ -17,4 +22,4 @@ navHamburger.addEventListener("click", function() {
     navMenu.classList.add("nav-is-active");
     navMenu.classList.remove("nav-not-active");
   }
-})
+}
